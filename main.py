3d75766baca6bf3d93b01355aea3d567ba8ca523
Sean Yo <sean.yo@gmail.com>
2025-03-28 16:30:50 -0300

    plot traversal is working but not as intended.

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+#Clear the farm
+clear()
+
+#Harvest the Farm
+
 while True:
-    move(North)
-    harvest()
+    for row in range(get_world_size()):
+        for column in range(get_world_size()):
+
+            if can_harvest():
+                harvest()
+                plant(Entities.Bush)
+                move(East)
+        move(North)
+
