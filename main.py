3959b2b9474d9f790ebf0541edff47bb75ec1c40
Sean Yo <sean.yo@gmail.com>
2025-03-28 16:48:45 -0300

    syntax fix

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ clear()
 
 # TODO: Drone skips first two harvests - looks like the can_harvest value is returning false. No idea why. This is sufficient for now. 
 while True:
-    for column in range(get_world_size()): #Loop over columns first
-            for row in range(get_world_size()): #Loop over rows in each column
+    for column in range(get_world_size()): #Loop over each column
+            for row in range(get_world_size()): #Loop over each row in each column
+                 
+                if can_harvest():
+                    harvest()
+                    plant(Entities.Bush)
 
-            if can_harvest():
-                harvest()
-                plant(Entities.Bush)
-
-        move(North)
+            move(North)
     move(East)
 
