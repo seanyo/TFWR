23ba637423070feeaf262ef530132c200fcad4a7
Sean Yo <sean.yo@gmail.com>
2025-03-28 16:40:01 -0300

    Drone movement implemented - incomplete but sufficient

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ clear()
 
 #Harvest the Farm
 
+# TODO: Drone skips first two harvests - looks like the can_harvest value is returning false. No idea why. This is sufficient for now. 
 while True:
-    for row in range(get_world_size()):
-        for column in range(get_world_size()):
+    for column in range(get_world_size()): #Loop over columns first
+            for row in range(get_world_size()): #Loop over rows in each column
 
             if can_harvest():
                 harvest()
                 plant(Entities.Bush)
-                move(East)
+
         move(North)
+    move(East)
 
